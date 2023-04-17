c0a23731198441b4371451e5798a45d324c4e990
cactusflashes <121457318+cactusflashes@users.noreply.github.com>
2023-04-16 17:18:51 -0700

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,7 +8,7 @@
 </script>
 
 <style>
-  @import '/Users/jas2598/Desktop/sunday /my-app/src/app.css';
+  @import 'src/app.css';
 
   @import url('https://fonts.googleapis.com/css2?family=Crimson+Text:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&family=Plus+Jakarta+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300&family=Poppins:ital,wght@0,100;0,200;0,400;0,500;0,700;1,300&display=swap');
 </style>
